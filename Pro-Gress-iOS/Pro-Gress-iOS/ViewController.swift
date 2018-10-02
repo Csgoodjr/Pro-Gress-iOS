5725bc1e84e4ceea3824a7ab3816bee8ce9a8f47
CJ Good <csg67@drexel.edu>
2018-10-02 13:45:42 -0400

    Initial Xcode Project

---
+++ Pro-Gress-iOS/Pro-Gress-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pro-Gress-iOS
+//
+//  Created by CJ Good on 10/2/18.
+//  Copyright © 2018 CJ Good. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
